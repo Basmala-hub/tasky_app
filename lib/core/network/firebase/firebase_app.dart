283d46fdf741e54aa50d9bda7670f6e04e86d995
Basmala-hub <basmala4198615@gmail.com>
2026-04-23 22:23:59 +0200

    Add user management functions to FireBase class

--- lib/core/network/firebase/firebase_app.dart
+++ lib/core/network/firebase/firebase_app.dart
@@ -2,9 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tasky/core/network/data/state_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tasky/features/auth/data/user_model.dart';
+import 'package:tasky/features/home/data/task_model.dart';
 
  class FireBase {
-
+//! create collection reference with converter
  static CollectionReference<UserdData> get _getCollection {
   return FirebaseFirestore.instance
       .collection("users")
@@ -14,10 +15,20 @@ import 'package:tasky/features/auth/data/user_model.dart';
         toFirestore: (user, _) => user.toJson(),
       );
 }
-
+//?add user function
 static Future<void> addUser(UserdData user) async {
   await _getCollection.doc(user.id).set(user);
 }
+//! get user function
+static Future<UserdData?> getUser(String id) async {
+  final doc = await _getCollection.doc(id).get();
+  return doc.data();
+}
+// static Future<void> addTask(TaskModel task)async{
+// await  _getCollection.doc(UserData.).set( task.tojson(task));
+// }
+
+//!register function
   static Future<StateModel> register({
     required String email,
     required String password,
@@ -38,7 +49,7 @@ static Future<void> addUser(UserdData user) async {
       return Erorr(e.toString());
     }
   }
-
+//!login function
   static Future<StateModel> login({
     required String email,
     required String password,
